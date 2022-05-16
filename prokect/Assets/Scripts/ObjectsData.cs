39b1d86da42da0e8a8b8e3be58d4672d17ac199d
dimastrik-78 <78548398+dimastrik-78@users.noreply.github.com>
2022-05-16 10:19:23 +0300

    Update ObjectsData.cs

--- prokect/Assets/Scripts/ObjectsData.cs
+++ prokect/Assets/Scripts/ObjectsData.cs
@@ -8,7 +8,6 @@ using UnityEngine.SceneManagement;
 public class ObjectsData : MonoBehaviour
 {
     public Transform canvas;
-    //[SerializeField] private GameObject itemPrefab;
     public GameObject[] ArrayImageItem;
     public ItemInfo[] Iitem;
     [HideInInspector] public int RandomNum;
@@ -29,28 +28,8 @@ public class ObjectsData : MonoBehaviour
         if (InventoryLen < 21)
         {
             ArrayImageItem[InventoryLen].SetActive(true);
-            //Iitem.GetComponent<ItemInfo>();
             RandomNum = Random.Range(0, 12);
-
-            //Debug.Log("ran = " + ran);
-            //Instantiate(itemPrefab, canvas.GetChild(0));
-            //canvas.GetChild(0).GetChild(InventoryLen).GetComponent<GameObject>().SetActive(true);
-
-            //Iitem.Item = itemtype[ran];
-
-
-
-
             Iitem[InventoryLen].GetComponent<Image>().sprite = itemtype[RandomNum].Icon;
-            //Iitem. = itemtype[1].Icon;
-            //Iitem = itemtype[1].ItemName;
-            //Iitem = itemtype[1].name;
-            //Iitem = itemtype[1].ItemType;
-            //Iitem = itemtype[1].ItemSpecial;
-            //Iitem.icon = itemtype[1].Icon;
-            //Iitem.Item = itemtype[ran];
-
-
             InventoryLen++;
         }
     }
@@ -58,33 +37,4 @@ public class ObjectsData : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
-    //[SerializeField] private GameObject itemPrefab;
-    //private int howManyItemsCreated = 0;
-    //private int rnd;
-    //[Range(1, 10)]
-    //[SerializeField] private int howManyItems;
-    //private Object[] itemDataHelp;
-    //[HideInInspector] public Objects[] itemData;
-
-    //private void Start()
-    //{
-    //    itemDataHelp = Resources.LoadAll("Items", typeof(Objects));
-    //    itemData = new Objects[itemDataHelp.Length];
-    //    for (int i = 0; i < itemDataHelp.Length; i++)
-    //    {
-    //        itemData[i] = (Objects)itemDataHelp[i];
-    //    }
-    //}
-
-    //public void OpenChaest()
-    //{
-    //    if (howManyItemsCreated < howManyItems)
-    //    {
-    //        rnd = Random.Range(0, 10);
-    //        itemPrefab.GetComponent<Image>().sprite = itemData[rnd].Icon;
-    //        Instantiate(itemPrefab, transform.GetChild(0));
-    //        ItemInfo.whereToPutDataSaver = itemData[rnd];
-    //        howManyItemsCreated++;
-    //    }
-    //}
 }
